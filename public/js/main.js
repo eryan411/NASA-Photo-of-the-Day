8f7ef261a8427409522f0cadc1a77d02d7096d1f
eryan411 <eryan411@gmail.com>
2022-07-30 13:33:23 -0400

    changed project to NodeJS and updated design

---
+++ public/js/main.js
@@ -0,0 +1,35 @@
+//Example fetch using pokemonapi.co
+document.querySelector('.getPhoto').addEventListener('click', getFetch)
+document.querySelector('.toggle').addEventListener('click', toggleShip)
+
+function getFetch(){
+  const choice = document.querySelector('input').value
+  const url = `https://api.nasa.gov/planetary/apod?api_key=iJy0Nd4wZZzfW2HFgBkNV4DDUsZ6PxL3RqQRgFfI&date=${choice}`
+
+  fetch(url)
+      .then(res => res.json()) // parse response as JSON
+      .then(data => {
+        console.log(data)
+        if (data.media_type === 'image'){
+          document.querySelector('iframe').src = ''
+          document.querySelector('.space').src = data.hdurl
+        } else if (data.media_type === 'video'){
+          document.querySelector('.space').src = ''
+          document.querySelector('iframe').src = data.url
+        }
+        document.querySelector('h2').innerText = data.title
+        document.querySelector('h3').innerText = data.explanation
+      })
+      .catch(err => {
+          console.log(`error ${err}`)
+      });
+}
+
+function toggleShip(){
+  let x = document.getElementById("spaceship");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const PORT = process.env.PORT || 8000
+
+const app = express()
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'))
+
+app.get('/', (req, res)=>{
+    res.render('index.ejs')
+})
+
+app.listen(PORT, () =>{
+    console.log(`Your server is running on Port ${PORT}, better go catch it!`)
+})
